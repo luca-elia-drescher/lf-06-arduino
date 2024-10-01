0bf784b568d6c26814a207e0e704b6e48bcd814e
lucaDrescher <lucaelia2003@gmail.com>
2024-10-01 11:31:01 +0200

    Added comments to voltage-bargraph.ino for submission

--- voltage-bargraph/voltage-bargraph.ino
+++ voltage-bargraph/voltage-bargraph.ino
@@ -1,43 +1,51 @@
+// Initialize pin positions for RGB LED
 int red = 3;
 int green = 6;
 int blue = 5;
 
-// max = 5V = 1023; min = 0V = 0
-// to determine the step size we calculate max divided by 5
-// step = 5V/5 = 1V; step= 1023/5 = 204,6
+/*
+  max = 5V = 1023; min = 0V = 0
+  to determine the step size we calculate max divided by 5
+  step = 5V/5 = 1V; step= 1023/5 = 204,6
+*/
 
+// Setup function for pin modes and serial communication
 void setup() {
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
-
-  pinMode(8, INPUT_PULLUP);
-  Serial.begin(9600);
+  
+  pinMode(8, INPUT_PULLUP); // Set pin 8 as input with pull-up resistor
+  Serial.begin(9600); // Begin serial communication
 }
 
 void loop() {
+  // Turn off all colors initially
   analogWrite(red, 0);
   analogWrite(green, 0);
   analogWrite(blue, 0);
 
-  if (digitalRead(8) == 0) {
-    int value = analogRead(5);
-    double voltage = value/204.6;
-    Serial.println(voltage);
-    delay(10);
+  if (digitalRead(8) == 0) { // Check if button (pin 8) is pressed
+    int value = analogRead(5); // Read analog value from pin A5
+    double voltage = value / 204.6; // Convert analog value to voltage
+    Serial.println(voltage); // Print voltage value
+    delay(10); // Small delay to avoid rapid polling
+
+    // Set LED color based on voltage range
     if (voltage > 1 && voltage < 2) {
         analogWrite(blue, 255);
         Serial.println("blue");
     } else if (voltage >= 2 && voltage < 4) {
         analogWrite(green, 255);
-      Serial.println("green");
+        Serial.println("green");
     } else if (voltage >= 4) {
         analogWrite(red, 255);
         Serial.println("red");
     } else {
-      analogWrite(red, 0);
-      analogWrite(green, 0);
-      analogWrite(blue, 0);
+        // Turn off all LEDs if voltage doesn't match any range
+        analogWrite(red, 0);
+        analogWrite(green, 0);
+        analogWrite(blue, 0);
     }
   }
 }
